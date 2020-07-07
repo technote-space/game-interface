44add5688333ad8fc0120edfd82a1105c237fb21
Technote <technote.space@gmail.com>
2020-07-07 16:59:08 +0900

    feat: implement

--- src/IGame.ts
+++ src/IGame.ts
@@ -1,5 +1,17 @@
 import {GameSettings} from './types';
 
 export interface IGame {
-  getSettings(): GameSettings;
+  step: number;
+  settings: GameSettings;
+  hasReached: boolean;
+
+  reset(): Promise<void>;
+
+  action(index: number): Promise<void>;
+
+  perceive(index: number): Promise<boolean>;
+
+  getScore(): number;
+
+  draw(target: string | HTMLCanvasElement): void;
 }
